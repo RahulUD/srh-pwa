efd254e8bf6dfa64b935ebea5c5cfe2e933e0178
Rahul <rahul.adsps@outlook.com>
2022-10-27 07:46:17 +0530

    role based auth added

--- src/app/guard/auth.guard.ts
+++ src/app/guard/auth.guard.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
+import { baseUser } from 'src/types/user';
 import { UserService } from '../service/user.service';
 
 @Injectable({
@@ -8,14 +9,22 @@ import { UserService } from '../service/user.service';
 })
 export class AuthGuard implements CanActivate {
   isLoggedIn: boolean = false
+  authUser!: baseUser|null
+
   constructor(private userService : UserService, private router: Router){
     this.userService.isLoggedIn$.subscribe(isLoggedin=> this.isLoggedIn = isLoggedin)
+    this.userService.authUser$.subscribe(authUser=> this.authUser = authUser)
   }
   canActivate(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
       if (this.isLoggedIn) {
-        return true;
+        if(this.authUser?.provider === 'admin'){
+          return true;
+        }else{
+          this.router.navigate(['/']);
+          return true
+        }
       } else {
         this.router.navigate(['/login']);
         return false;

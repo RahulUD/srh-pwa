99c4b2191c21540fc82c61f31f688cc502087ac2
Rahul <rahul.adsps@outlook.com>
2022-11-05 07:16:47 +0530

    updating prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,7 +3,6 @@ declare const require : any;
 
 // const baseUrl = 'http://139.59.35.248:8000/'
 const baseUrl = 'http://localhost:8000/'
-
 export const environment = {
   production: false,
   appVersion: require('../../package.json').version,

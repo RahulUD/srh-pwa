de5372e4044c382345d6afc9cadca12fc1a7ecf0
Rahul <rahul.adsps@outlook.com>
2022-11-11 07:57:19 +0530

    signup api integrated

---
+++ src/app/util/textConstant.ts
@@ -0,0 +1,5 @@
+// Auth
+export const signInText = 'Already have an account? Sign In.';
+export const signUpText = 'New here? Create an account.';
+export const passwordHelpText = 'Keep the password strong.'
+export const resetPasswordText = 'Reset Password'

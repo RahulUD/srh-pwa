ef2213baa3563bdebd478d34fcdec20069a0c4af
Rahul <rahul.adsps@outlook.com>
2022-11-26 14:09:40 +0530

    updating baseurl

---
+++ startup.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+date > /home/rahul/api-runner/_work/sarkariresulthub-api/sarkariresulthub-api/disk_space_report.txt
+du -sh /home/ >> /home/rahul/api-runner/_work/sarkariresulthub-api/sarkariresulthub-api/disk_space_report.txt
